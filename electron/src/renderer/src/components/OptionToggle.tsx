d9873a6eefdcb6870b2cb371b337d7acd44a5ecd
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-11-03 21:33:32 +0100

    Wrap className of div in OptionToggle

--- electron/src/renderer/src/components/OptionToggle.tsx
+++ electron/src/renderer/src/components/OptionToggle.tsx
@@ -12,7 +12,13 @@ export default function OptionToggle({ title, option1, option2 }: OptionTogglePr
         <label className="inline-flex items-center cursor-pointer">
           <span className="mr-3 text-xs font-medium">{option1}</span>
           <input type="checkbox" value="" className="sr-only peer" />
-          <div className="relative w-11 h-6 bg-blue-600 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-blue-600"></div>
+          <div
+            className="relative w-11 h-6 bg-blue-600 peer-focus:outline-none rounded-full 
+            peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
+            peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
+            after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
+            after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-blue-600">
+          </div>
           <span className="ml-3 text-xs font-medium">{option2}</span>
         </label>
       </div>
