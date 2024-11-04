17fb34d0b1f464939e7613ee9bbb166ca799dda7
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-11-04 22:29:25 +0100

    Lowercase year format and change title

--- electron/src/renderer/src/components/Options.tsx
+++ electron/src/renderer/src/components/Options.tsx
@@ -3,7 +3,7 @@ import OptionToggle from "./OptionToggle"
 export default function Options() {
   return (
     <div className="mt-10 w-full">
-      <OptionToggle id="year-option-toggle" title="Year Option" option1="YYYY" option2="YY" isOnOff={false} />
+      <OptionToggle id="year-option-toggle" title="Year Format" option1="yyyy" option2="yy" isOnOff={false} />
       <OptionToggle className="mt-5" id="year-option-toggle" title="Display Time" option1="Off" option2="On" isOnOff={true} />
     </div>
   )
