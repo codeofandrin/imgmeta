d56b53fe5fad8c3b8f18f4beb335ab00dd663e1c
Andrin <65789180+Puncher1@users.noreply.github.com>
2024-11-09 15:30:56 +0100

    Use caps for year format options

--- electron/src/renderer/src/components/Options.tsx
+++ electron/src/renderer/src/components/Options.tsx
@@ -3,7 +3,7 @@ import OptionToggle from "./OptionToggle"
 export default function Options() {
   return (
     <div className="mt-10 w-full">
-      <OptionToggle id="year-option-toggle" title="Year Format" option1="yyyy" option2="yy" isOnOff={false} />
+      <OptionToggle id="year-option-toggle" title="Year Format" option1="YYYY" option2="YY" isOnOff={false} />
       <OptionToggle className="mt-5" id="time-option-toggle" title="Display Time" isOnOff={true} />
     </div>
   )
